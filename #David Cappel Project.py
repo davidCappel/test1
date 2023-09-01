691d80c853e88a38ac8ca6b199d90c86f6a2c5ad
davidCappel <117950530+davidCappel@users.noreply.github.com>
2023-09-01 10:55:57 -0400

    more testing

--- #David Cappel Project.py
+++ #David Cappel Project.py
@@ -1,6 +1,9 @@
 #David Cappel Project
-g= str(input("What is my name:"))
-if g[0] == "d":
-    print("Must be capitol")
-elif g[0] == "D":
-    print("perfect")
+g= str(input())
+def sequence():
+    print("What is my name?")
+    if g[0] == "d":
+        print("Must be capitol")
+        sequence()
+    elif g[0] == "D":
+        print("perfect")
